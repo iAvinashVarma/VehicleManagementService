5969d6aa585bbdcba7af766d60bcb237b9d72a99
Avinash Varma Kalidindi <iavinashvarma@gmail.com>
2020-04-13 10:09:02 +1200

    Add missing test case.

---
+++ Middle/VehicleManagementSystemBusiness.Tests/Model/VehiclePressureTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using VehicleManagementSystemBusiness.Model;
+
+namespace VehicleManagementSystemBusiness.Tests.Model
+{
+    [TestFixture]
+    public class VehiclePressureTest
+    {
+        [Test]
+        public void CheckPressurePsiVsPascal()
+        {
+            var expectedPressurePascal = 206842.80000000002D;
+            var givenPressurePsi = 30.0D;
+            var vehiclePressure = new VehiclePressure
+            {
+                PressurePsi = givenPressurePsi
+            };
+
+            double actualPressurePascal = vehiclePressure.PressurePascal;
+
+            Assert.AreEqual(expectedPressurePascal, actualPressurePascal);
+        }
+    }
+}
